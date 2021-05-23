ee273f8c79ec6dd17ea18f19eacbab6b417d91da
Irina Fylyppova <irina.h.fylyppova@gmail.com>
2021-05-23 21:45:25 +0300

    initial date bug fixed

--- src/date-functions/initial.js
+++ src/date-functions/initial.js
@@ -1,6 +1,6 @@
 const dayDuration = 24 * 60 * 60 * 1000
 const weekDuration = 7 * 24 * 60 * 60 * 1000
-const initialDate = Date.parse(new Date(2018, 0, 1))
+const initialDate = Date.parse('2018-01-01')
 
 export {
   dayDuration,
